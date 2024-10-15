9cbd90ed8b41716aabda38691949b0544f178192
mrohith29 <mariyalarohith29@gmail.com>
2024-10-15 13:26:21 +0530

    edit port

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def upload_file():
         return "No file or question provided", 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
